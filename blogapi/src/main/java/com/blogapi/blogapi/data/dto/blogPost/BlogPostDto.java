cc7b2477263e5f9542e39e2f6abc509c9c66dfd7
João Paulo <jpcchaves@outlook.com>
2023-12-14 20:13:24 -0300

    feat: create dto pojo classes that is going to be used to transfer data

---
+++ blogapi/src/main/java/com/blogapi/blogapi/data/dto/blogPost/BlogPostDto.java
@@ -0,0 +1,78 @@
+package com.blogapi.blogapi.data.dto.blogPost;
+
+import java.util.Date;
+
+public class BlogPostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+
+    public BlogPostDto() {
+    }
+
+    public BlogPostDto(
+            Long id,
+            String title,
+            String description,
+            Date createdAt,
+            Date updatedAt,
+            Date deletedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
