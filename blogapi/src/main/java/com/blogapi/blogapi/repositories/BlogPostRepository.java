3d6cf2d6710e1e5a0bc7953a024ed2a536966570
João Paulo <jpcchaves@outlook.com>
2023-12-14 20:12:32 -0300

    feat: create blog post jpa repository

---
+++ blogapi/src/main/java/com/blogapi/blogapi/repositories/BlogPostRepository.java
@@ -0,0 +1,9 @@
+package com.blogapi.blogapi.repositories;
+
+import com.blogapi.blogapi.domain.entities.BlogPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+}
