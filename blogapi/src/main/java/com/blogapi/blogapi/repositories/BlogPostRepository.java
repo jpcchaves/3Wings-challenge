022372d8ca13153d1509aed456f8246d2250828f
João Paulo <jpcchaves@outlook.com>
2023-12-14 21:46:23 -0300

    feat: add custom jpa methods in blog post repository

--- blogapi/src/main/java/com/blogapi/blogapi/repositories/BlogPostRepository.java
+++ blogapi/src/main/java/com/blogapi/blogapi/repositories/BlogPostRepository.java
@@ -1,9 +1,21 @@
 package com.blogapi.blogapi.repositories;
 
 import com.blogapi.blogapi.domain.entities.BlogPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+
+    @Query("SELECT bp FROM BlogPost bp WHERE bp.isDeleted = false")
+    Page<BlogPost> findAllBlogPosts(Pageable pageable);
+
+    @Query("SELECT bp FROM BlogPost bp WHERE bp.id = :id AND bp.isDeleted = false")
+    Optional<BlogPost> findBlogPostById(@Param("id") Long id);
 }
