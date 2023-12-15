df38a1be655c45a0a04f4192054f955aa62a7c67
jpcchaves <joao.chaves@liferay.com>
2023-12-15 09:11:01 -0300

    feat: add all crud operations in useblogposts hook

--- blogfront/src/modules/blogPosts/hooks/useBlogPosts.tsx
+++ blogfront/src/modules/blogPosts/hooks/useBlogPosts.tsx
@@ -1,9 +1,12 @@
 import { blogPostEndpoint } from "../../../constants/env";
+import { BlogPostCreateRequestDto } from "../../../domain/models/blogPosts/BlogPostCreateRequestDto";
+import { BlogPostDto } from "../../../domain/models/blogPosts/BlogPostDto";
 import { BlogPostMinDto } from "../../../domain/models/blogPosts/BlogPostMinDto";
+import { ApiMessageResponse } from "../../../domain/models/common/ApiMessageResponse";
 import { ApiPaginatedResponse } from "../../../domain/models/common/ApiPaginatedResponse";
 import useLoading from "../../../hooks/loading/useLoading";
 import { useAppDispatch } from "../../../hooks/useRedux";
-import { loadBlogPostsPaginated } from "../../../store/blogPosts";
+import { loadBlogPost, loadBlogPostsPaginated } from "../../../store/blogPosts";
 import { HttpMethod, httpRequest } from "../../../utils/http";
 
 const useBlogPosts = () => {
@@ -26,7 +29,87 @@ const useBlogPosts = () => {
       });
   };
 
-  return { getBlogPostList, isLoading };
+  const getBlogPostById = async (id: string) => {
+    setLoading(true);
+    await httpRequest<void, BlogPostDto>(
+      HttpMethod.GET,
+      `${blogPostEndpoint}/${id}`
+    )
+      .then((res) => {
+        dispatch(loadBlogPost(res));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const createBlogPost = async (data: BlogPostCreateRequestDto) => {
+    setLoading(true);
+    await httpRequest<BlogPostCreateRequestDto, ApiMessageResponse>(
+      HttpMethod.POST,
+      blogPostEndpoint,
+      data
+    )
+      .then((res) => {
+        console.log(res);
+        getBlogPostList();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const updateBlogPost = async (data: BlogPostCreateRequestDto, id: string) => {
+    setLoading(true);
+    await httpRequest<BlogPostCreateRequestDto, ApiMessageResponse>(
+      HttpMethod.PUT,
+      `${blogPostEndpoint}/${id}`,
+      data
+    )
+      .then((res) => {
+        console.log(res);
+        getBlogPostList();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const deleteBlogPost = async (id: string) => {
+    setLoading(true);
+    await httpRequest<void, ApiMessageResponse>(
+      HttpMethod.DELETE,
+      `${blogPostEndpoint}/${id}`
+    )
+      .then((res) => {
+        console.log(res);
+        getBlogPostList();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return {
+    getBlogPostList,
+    getBlogPostById,
+    createBlogPost,
+    updateBlogPost,
+    deleteBlogPost,
+    isLoading,
+  };
 };
 
 export default useBlogPosts;
