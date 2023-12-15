92e6aec07bdba0390d6ad4a36300bf17aa0e8a8f
João Paulo <jpcchaves@outlook.com>
2023-12-14 22:24:22 -0300

    feat: add redux related configuration files

--- blogfront/src/main.tsx
+++ blogfront/src/main.tsx
@@ -1,12 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
+import { store } from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
