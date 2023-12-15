f0d5543a0f42cfe750e5c6bea691f3cb54d3ed17
jpcchaves <joao.chaves@liferay.com>
2023-12-15 09:00:59 -0300

    feat: implemented blog posts slice in get blog posts list

--- blogfront/src/modules/blogPosts/hooks/useBlogPosts.tsx
+++ blogfront/src/modules/blogPosts/hooks/useBlogPosts.tsx
@@ -1,26 +1,32 @@
 import { blogPostEndpoint } from "../../../constants/env";
 import { BlogPostMinDto } from "../../../domain/models/blogPosts/BlogPostMinDto";
 import { ApiPaginatedResponse } from "../../../domain/models/common/ApiPaginatedResponse";
+import useLoading from "../../../hooks/loading/useLoading";
+import { useAppDispatch } from "../../../hooks/useRedux";
+import { loadBlogPostsPaginated } from "../../../store/blogPosts";
 import { HttpMethod, httpRequest } from "../../../utils/http";
 
 const useBlogPosts = () => {
+  const { isLoading, setLoading } = useLoading();
+  const dispatch = useAppDispatch();
   const getBlogPostList = async () => {
+    setLoading(true);
     await httpRequest<void, ApiPaginatedResponse<BlogPostMinDto>>(
       HttpMethod.GET,
       blogPostEndpoint
     )
       .then((res) => {
-        console.log(res);
+        dispatch(loadBlogPostsPaginated(res));
       })
       .catch((err) => {
         console.log(err);
       })
       .finally(() => {
-        console.log("");
+        setLoading(false);
       });
   };
 
-  return { getBlogPostList };
+  return { getBlogPostList, isLoading };
 };
 
 export default useBlogPosts;
