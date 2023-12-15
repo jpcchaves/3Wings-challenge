4fea6bfeeab684d35b2d4d5a917e000f82e04c0d
jpcchaves <joao.chaves@liferay.com>
2023-12-15 08:51:45 -0300

    feat: add http helper methods, enums, and axios instance

---
+++ blogfront/src/utils/http/index.ts
@@ -0,0 +1,5 @@
+import { axiosInstance } from "./axiosInstance";
+import { HttpMethod } from "./httpMethods";
+import { httpRequest } from "./httpRequest";
+
+export { HttpMethod, axiosInstance, httpRequest };
